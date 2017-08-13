bffc93142402c9babae217b87e4da7c898529d82
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2017-08-13 15:47:28 +0200

    handled Database exception

--- src/main/scala/actors/DatabaseManagerActor.scala
+++ src/main/scala/actors/DatabaseManagerActor.scala
@@ -97,36 +97,68 @@ class DatabaseManagerActor extends UntypedAbstractActor {
 
   //todo: da testare
   private def getMatchResultFor(username: String): Option[List[Map[String, Any]]] = {
-    val results: List[MatchResult] = DatabaseQuery.allMatches(username)
-    var list: List[Map[String, Any]] = List()
+    try {
+      val results: List[MatchResult] = DatabaseQuery.allMatches(username)
+      var list: List[Map[String, Any]] = List()
 
-    results.foreach((x) => {
-      val result: Map[String, Any] = Map("result" -> x.result ,"score" -> x.score, "date" -> x.date)
-      list = list ::: List(result)
-    })
+      results.foreach((x) => {
+        val result: Map[String, Any] = Map("result" -> x.result, "score" -> x.score, "date" -> x.date)
+        list = list ::: List(result)
+      })
 
       Option(list)
+    }
+    catch {
+      case e: ExceptionInInitializerError => {
+        println("Database not initialized ! Open DB connection")
+        Option.empty[List[Map[String, Any]]]
+      }
+    }
   }
 
   // todo: da testare
   private def addUserToDB(user: User): Boolean = {
-    DatabaseQuery addUser(user.name,user.username,user.mail,user.password)
+    try {
+      DatabaseQuery addUser(user.name, user.username, user.mail, user.password)
+    }
+    catch {
+      case e: ExceptionInInitializerError => {
+        println("Database not initialized ! Open DB connection")
+        false
+      }
+    }
   }
 
   //Todo: da testare
   private def checkLoginInfo(username: String, password: String): Boolean = {
-    val result: String = DatabaseQuery checkLogin(username, password)
+    try {
+      val result: String = DatabaseQuery checkLogin(username, password)
 
-    result match {
-      case "logged" => true
-      case "passwordWrong" => false
-      case "unregisteredUsername" => false
+      result match {
+        case "logged" => true
+        case "passwordWrong" => false
+        case "unregisteredUsername" => false
+      }
+    }
+    catch {
+      case e: ExceptionInInitializerError => {
+        println("Database not initialized ! Open DB connection")
+        false
+      }
     }
   }
 
   //todo: da testare
   private def addResult(username: String, result: MatchResult): Boolean = {
-    DatabaseQuery addMatchResult(username,result)
+    try {
+      DatabaseQuery addMatchResult(username, result)
+    }
+    catch {
+      case e: ExceptionInInitializerError => {
+        println("Database not initialized ! Open DB connection")
+        false
+      }
+    }
   }
 
 }
